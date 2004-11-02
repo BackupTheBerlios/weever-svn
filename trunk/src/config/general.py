bdb2eefcafc6fa89b15afd420b35f91bdb4d5539
dialtone <dialtone@a440c657-b6e6-0310-a3b3-b76a39be4160>
2004-11-02 15:10:17 +0000

    removed 'the old way' configuration

--- trunk/src/config/general.py
+++
@@ -1,7 +0,0 @@
-database = 'postgresql'
-database_adapter = 'psycopg'
-
-#parameters are:
-# dbname, host, user, password, sslmode, port
-database_dsn = 'dbname=weever user=testuser password=test'
-

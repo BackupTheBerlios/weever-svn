67847e0ea1b03821090ac9f4820cf5184c1c7382
dialtone <dialtone@a440c657-b6e6-0310-a3b3-b76a39be4160>
2004-11-03 23:32:18 +0000

    forgot one thing

--- trunk/src/utils/util.py
+++ trunk/src/utils/util.py
@@ -1,4 +1,6 @@
 import os.path as op
+from twisted.python import util
+
 def sibpath(from, to):
     parent = op.split(op.split(from)[0])[0]
     if op.isfile(parent): path = util.sibpath(parent, to)
